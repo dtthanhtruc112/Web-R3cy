4d173eac68409aedbfcfe734d8d18b937fe742da
holethanhtruc <truchlt21411@st.uel.edu.vn>
2023-12-16 14:12:54 +0700

    update product-cart

--- DoanR3cy/src/app/product-cart/product-cart.component.ts
+++ DoanR3cy/src/app/product-cart/product-cart.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { product } from '../Interface/product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../Service/product.service';
+import { map, switchMap } from 'rxjs';
+
 
 @Component({
   selector: 'app-product-cart',
@@ -6,5 +11,19 @@ import { Component } from '@angular/core';
   styleUrl: './product-cart.component.css'
 })
 export class ProductCartComponent {
+  product: any;
+  errMsg: string ="";
+
+  selectedCode: any;
+  constructor(private productService: ProductService, private _router: Router, private _activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.productService.getData().subscribe((data: product[]) => {
+      this.product = data;
+    });
+  }
+
+
+  
 
 }
