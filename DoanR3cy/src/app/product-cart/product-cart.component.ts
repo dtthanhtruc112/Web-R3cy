d8f413e1cca8741858da894fb95ea7314011e292
holethanhtruc <truchlt21411@st.uel.edu.vn>
2023-12-20 13:42:38 +0700

    xóa sản phẩm

--- DoanR3cy/src/app/product-cart/product-cart.component.ts
+++ DoanR3cy/src/app/product-cart/product-cart.component.ts
@@ -12,11 +12,11 @@ import { CartService } from '../Service/cart.service';
   styleUrl: './product-cart.component.css'
 })
 export class ProductCartComponent {
-  
 
-  cartItems: product[] | undefined;
 
-  constructor(private cartService: CartService) {}
+  cartItems: product[] = [];
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit() {
     // Đăng ký để theo dõi sự thay đổi của giỏ hàng
@@ -32,7 +32,14 @@ export class ProductCartComponent {
     const subtotal = quantity * price;
 
     return subtotal;
-}
-
+  }
+  
+  //Xóa sản phẩm
+  removeItem(item: product): void {
+    const index = this.cartItems.indexOf(item);
+    if (index !== -1) {
+      this.cartItems.splice(index, 1);
+    }
+  }
 
 }
