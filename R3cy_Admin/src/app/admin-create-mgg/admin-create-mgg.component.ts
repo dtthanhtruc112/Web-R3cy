dc23678e3d599793c33ad2b36281eec55992c693
phuocng216 <phuocxyzz@example.com>
2024-01-01 00:11:16 +0700

    update css (chưa xong)

---
+++ R3cy_Admin/src/app/admin-create-mgg/admin-create-mgg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-create-mgg',
+  templateUrl: './admin-create-mgg.component.html',
+  styleUrl: './admin-create-mgg.component.css'
+})
+export class AdminCreateMggComponent {
+
+}
